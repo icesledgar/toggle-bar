7ca2a022d457ad0a402f18ef18427f1e1261c993
Aditya Prasad <138990468+icesledgar@users.noreply.github.com>
2024-01-09 20:05:04 +0530

    update

---
+++ rr.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int i ,f, NoP, sum = 0, count = 0, qyart, wt = 0, tat = 0, at [10],temp[10],bt[10];
+    float avg__wt, avg_tat;  
+    printf("Total no. of process in the system");
+    scan("%d", &NoP);
+    f = NoP;
+    for ( i = 0; i < NoP; i++)
+    {
+        printf("\n Enter the Arrival and Burst time of the Process [%d] \n", i+1);
+        printf("Arrival time is\t");
+        scanf("%f",&at[i]);
+        printf("Burst time")
+    }
+    
+
+}
